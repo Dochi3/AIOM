060883cbfe95266c42256e7afc1b6c5615da9b56
sunjbs <sunjbs98@gmail.com>
2019-02-11 19:48:54 +0900

    Fix : Omok.py     Fix : Omkok.checkVali(self, r, c)         >>> 금수(3x3) 추가
    오목 완성(추정)

--- Omok.py
+++ Omok.py
@@ -1,26 +1,38 @@
 import OmokValue
 class Omok:
     def __init__(self):
-        self.size = 19
+        self.size = 5
+        self.gameOver = True
         self.newGame()
 
     def display(self):
         displayString = str()
-        for row in range(self.size):
-            for col in range(self.size):
-                nowStatus = self.board[row][col] % 2
-                if not self.board[row][col]:
-                    displayString += " "
-                elif nowStatus == OmokValue.WHITE:
-                    displayString += "O"
-                else:
-                    displayString += "X"
-                displayString += "─"
-            displayString = displayString[:-1] + "\n"
-            for col in range(self.size):
-                displayString += "| "
-            displayString = displayString[:-1] + "\n"
-        displayString = displayString[:-39]
+        sliceSize = self.size * 2 + 1
+        if self.gameOver:
+            displayString = ("BLACK" if self.turn % 2 else "WHITE") + " win" + " " * sliceSize
+        else:
+            for row in range(self.size):
+                for col in range(self.size):
+                    nowStatus = self.board[row][col] % 2
+                    validity, omokCase = self.checkValid(row, col)
+                    if omokCase == OmokValue.CASE_3X3:
+                        displayString += "3"
+                    elif omokCase == OmokValue.CASE_4X4:
+                        displayString += "4"
+                    elif omokCase == OmokValue.CASE_OVER_6:
+                        displayString += "6"
+                    elif not self.board[row][col]:
+                        displayString += " "
+                    elif nowStatus == OmokValue.WHITE:
+                        displayString += "O"
+                    else:
+                        displayString += "X"
+                    displayString += "─"
+                displayString = displayString[:-1] + "\n"
+                for col in range(self.size):
+                    displayString += "| "
+                displayString = displayString[:-1] + "\n"
+        displayString = displayString[:-sliceSize]
         return displayString
 
     def newGame(self):
@@ -29,29 +41,31 @@ class Omok:
         self.gameOver = False
 
     def isValidPosition(self, r, c):
-        if r < 0 or r > 18:
+        if r < 0 or r > self.size - 1:
             return False
-        if c < 0 or c > 18:
+        if c < 0 or c > self.size - 1:
             return False
         return True
 
     def countStone(self, r, c, dir_x, dir_y):
-        count, counter = [0, 0], False
+        count = [0, 0]
         nowStatus, jump = self.turn % 2, 0
         while jump < 2:
             r, c = r + dir_x, c + dir_y
             if not self.isValidPosition(r, c):
+                jump = 2
                 break
             if not self.board[r][c]:
                 jump += 1
+                continue
             if self.board[r][c] % 2 != nowStatus:
-                counter = True
                 break
-        return (count, counter)
+            count[jump] += 1
+        return (count, jump)
 
     def checkValid(self, r, c):
         # start position
-        if self.turn == 1 and (r != 9 or c != 9):
+        if self.turn == 1 and (r != self.size // 2 or c != self.size // 2):
             return (False, OmokValue.START_POSITION_ERROR)
 
         # check about position is valid
@@ -65,21 +79,31 @@ class Omok:
         directions = [[0, 1], [1, 1], [1, 0], [1, -1]]
         case3x3, case4x4 = 0, 0
         for dir_x, dir_y in directions:
-            positive, p_counter = self.countStone(r, c, dir_x, dir_y)
-            negative, n_counter = self.countStone(r, c, -dir_x, -dir_y)
-            linearCount = positive[0] + negative[0]
+            positive, pCounter = self.countStone(r, c, dir_x, dir_y)
+            negative, nCounter = self.countStone(r, c, -dir_x, -dir_y)
+            linearCount, jumpCount = positive[0] + negative[0], positive[1] + negative[1]
             if linearCount == 4:
                 return (True, OmokValue.CASE_OMOK)
 
             # white stone has no abandonment
             if self.turn % 2 == OmokValue.WHITE:
                 continue
-            
+
             # case Over 6
             if linearCount > 5:
                 return (False, OmokValue.CASE_OVER_6)
 
             # case 3x3
+            if pCounter * nCounter == 0:
+                continue
+            elif not jumpCount and linearCount == 2:
+                case3x3 += 1
+            elif (positive[1] and not negative[1]
+                  and linearCount + positive[1] == 2 and pCounter > 1):
+                case3x3 += 1
+            elif (negative[1] and not positive[1]
+                  and linearCount + negative[1] == 2 and nCounter > 1):
+                case3x3 += 1
 
             # case 4x4
             if positive[1] + negative[1] == 0 and linearCount == 3:
@@ -120,7 +144,7 @@ if __name__ == "__main__":
     displayClear = lambda: system("clear")
     while True:
         displayClear()
-        print(omok.display())
+        print(omok.display(), flush=True)
         position = input("Enter pos : ")
         if position == "exit":
             break
